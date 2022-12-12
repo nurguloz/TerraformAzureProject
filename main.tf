31703d2fc5c4e4c63f2d5c9c1490f0633e5aebcb
nurgul ozturk <114959922+nurguloz@users.noreply.github.com>
2022-12-12 14:23:43 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.35.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    
+  }
+}
